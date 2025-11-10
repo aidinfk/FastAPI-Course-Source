ba84573572b6e477759169bdc322066cb7a73302
aidinfk <aydinfakhri9@gmail.com>
2025-11-10 20:16:34 +0330

    Simplify code with helper functions

---
+++ 011/crud.py
@@ -0,0 +1,17 @@
+from exceptions import EmailAlreadyExistsException, UserNotFoundException
+from schemas import UserOutput
+
+
+def get_user_or_404(users: list[UserOutput], user_id: int):
+    existing_user = next((u for u in users if u.id == user_id), None)
+    if not existing_user:
+        raise UserNotFoundException()
+    return existing_user
+
+def email_already_exists(users: list[UserOutput], email: str):
+    if any(u.email == email for u in users):
+        raise EmailAlreadyExistsException()
+
+def email_already_exists_update(users: list[UserOutput], email: str, existing_user:UserOutput):
+    if any(u.email == email and u.email != existing_user.email for u in users):
+        raise EmailAlreadyExistsException()
